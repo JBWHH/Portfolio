f81583cb37da656238f6cede5c790c67770e6ffd
jbwh <joelbawuah@hotmail.com>
2025-04-23 20:23:52 -0400

    added nav ids to sections

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -50,20 +50,10 @@ const Contact = () => {
   };
 
   return (
-    <section className="c-space my-20">
+    <section className="c-space my-20" id="contact">
       <div className="relative min-h-screen flex items-center justify-center flex-col">
-        {/* <img
-          src="/assets/terminal.png"
-          alt="terminal background"
-          className="absolute inset-0 min-h-screen"
-        /> */}
         <div className="contact-container">
           <h3 className="head-text">Let's Talk</h3>
-          <p className="text-lg text-white-600">
-            Whether you're looking to build a new website, improve you're
-            existing platform, or bring a unique project to life, I'm here to
-            help.{" "}
-          </p>
           <form
             ref={formRef}
             onSubmit={handleSubmit}
