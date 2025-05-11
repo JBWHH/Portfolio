167c4e4f7bb5d811996be70cd06ee5a3c9daa80c
jbwh <joelbawuah@hotmail.com>
2025-05-10 21:52:57 -0400

    update about and Hero section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,19 +3,10 @@ import Globe from 'react-globe.gl'
 import Button from '../Components/Button'
 
 const About = () => {
-    const [hasCopied, setHasCopied] = useState(false);
-    const handleCopy = () => {
-        navigator.clipboard.writeText("joelbawuah1@gmail.com");
-        setHasCopied(true);
-
-        setTimeout(() => {
-            setHasCopied(false);
-        }, 2000);
-    };
 
   return (
     <section className="c-space my-20" id="about">
-        <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="grid xl:grid-cols-2 xl:grid-rows-2 md:grid-cols-2 grid-cols-1 gap-5 h-full">
             <div className="col-span-1 xl:row-span-3">
                 <div className="grid-container">
                     <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
@@ -34,27 +25,6 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className="col-span-1 xl:row-span-4">
-                <div className="grid-container">
-                    <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                        <Globe
-                            height={326}                       
-                            width={326}
-                            backgroundColor="rgba(0,0,0,0)"
-                            backgroundImageOpacity={0.5}
-                            showAtmosphere
-                            showGraticules
-                            globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                            bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"                   
-                        />
-                    </div>
-                    <div>
-                        <p className="grid-headtext">I work remotely across most timezones.</p>
-                        <p className="grid-subtext">I'm based in Canada, with remote work available.</p>
-                        <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
-                    </div>
-                </div>
-            </div>
             <div className="xl:col-span-2 xl:row-span-3">
                 <div className="grid-container">
                     <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
@@ -63,20 +33,6 @@ const About = () => {
                         <p className="grid-subtext">I love solving problems and building through code. Coding isn't just a job for me, it's a way of life.</p>
                     </div>
                 </div>  
-            </div>
-            <div className="xl:col-span-1 xl:row-span-2">
-                <div className="grid-container">
-                    <img src="/assets/grid4.png" alt="grid-4" className="w-full sm:h-[276px] h-fit object-cover sm:object-top" />
-                    <div className="space-y-2">
-                        <p className="grid-subtext text-center">Contact Me</p>
-                        <div className="copy-container" onClick={handleCopy}>
-                            <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
-                            <p className="lg:text-1xl md:text-xl font-medium text-gray_gradient text-white">
-                                joelbawuah1@gmail.com
-                            </p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,10 +26,10 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-          Hi, I am Joel <span className="waving-hand">👋</span>
+         Hi, I'm Joel Bawuah<span className="waving-hand">👋</span>
         </p>
         <p className="hero_tag text-gray_gradient">
-          Building Products & Brands
+          Welcome to my portfolio 
         </p>
       </div>
 
